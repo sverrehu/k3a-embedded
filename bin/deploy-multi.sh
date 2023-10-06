e69c40dda28d78247c1a2c373862d69527dc2db9
Sverre H Huseby <shh@thathost.com>
2023-10-06 18:56:16 +0200

    feat: build for multiple Kafka versions

---
+++ bin/deploy-multi.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if test \! -f pom.xml
+then
+    echo "Must be run from the directory of the pom.xml file"
+    exit 1
+fi
+
+KAFKA_VERSIONS="3.6.0 3.5.1"
+CURRENT_VERSION="$(grep '<version>' pom.xml | head -1 | sed -re 's/[[:space:]]*<version>(.*)<\/version>[[:space:]]*/\1/')"
+
+mvn --batch-mode deploy
+cp pom.xml pom.xml.old
+for KAFKA_VERSION in $KAFKA_VERSIONS
+do
+    NEW_VERSION="$CURRENT_VERSION"+"$KAFKA_VERSION"
+    echo "$NEW_VERSION"
+    mvn versions:set -DnewVersion="$NEW_VERSION"
+    mvn versions:commit
+    sed -i~ -re 's/<kafka.version>.*<\/kafka.version>/<kafka.version>'"$KAFKA_VERSION"'<\/kafka.version>/' pom.xml
+    mvn --batch-mode deploy
+done
+mv pom.xml.old pom.xml
+rm -f pom.xml.versionsBackup

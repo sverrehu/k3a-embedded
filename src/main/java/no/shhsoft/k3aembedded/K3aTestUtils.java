8e408e395cb8b41952aec2e14ac5cb959e0d90a0
Sverre H. Huseby <shh@thathost.com>
2023-10-24 14:36:49 +0200

    feat: add test helper methods (#54)
    * feat: add test helper methods
    
    * use String as value

---
+++ src/main/java/no/shhsoft/k3aembedded/K3aTestUtils.java
@@ -0,0 +1,96 @@
+package no.shhsoft.k3aembedded;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/* Inspired by / stolen from org.springframework.kafka.test.utils.KafkaTestUtils */
+public final class K3aTestUtils {
+
+    private K3aTestUtils() {
+    }
+
+    /**
+     * Set up test properties for an {@code <Integer, String>} consumer.
+     * @param group the group id.
+     * @param autoCommit the auto commit.
+     * @param k3aEmbedded a {@link K3aEmbedded} instance.
+     * @return the properties.
+     */
+    public static Map<String, Object> consumerProps(final String group, final boolean autoCommit, final K3aEmbedded k3aEmbedded) {
+        return consumerProps(k3aEmbedded.getBootstrapServers(), group, autoCommit);
+    }
+
+    /**
+     * Set up test properties for an {@code <Integer, String>} producer.
+     * @param k3aEmbedded a {@link K3aEmbedded} instance.
+     * @return the properties.
+     */
+    public static Map<String, Object> producerProps(final K3aEmbedded k3aEmbedded) {
+        return producerProps(k3aEmbedded.getBootstrapServers());
+    }
+
+    /**
+     * Set up test properties for admin client.
+     * @param k3aEmbedded a {@link K3aEmbedded} instance.
+     * @return the properties.
+     */
+    public static Map<String, Object> adminProps(final K3aEmbedded k3aEmbedded) {
+        return adminProps(k3aEmbedded.getBootstrapServers());
+    }
+
+    /**
+     * Set up test properties for an {@code <Integer, String>} consumer.
+     * @param bootstrapServers the bootstrapServers property.
+     * @param group the group id.
+     * @param autoCommit the auto commit.
+     * @return the properties.
+     */
+    public static Map<String, Object> consumerProps(final String bootstrapServers, final String group, final boolean autoCommit) {
+        final Map<String, Object> map = new HashMap<>();
+        map.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        map.put(ConsumerConfig.GROUP_ID_CONFIG, group);
+        map.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, autoCommit);
+        map.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "10");
+        map.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        map.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        map.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return map;
+    }
+
+    /**
+     * Set up test properties for an {@code <Integer, String>} producer.
+     * @param bootstrapServers the bootstrapServers property.
+     * @return the properties.
+     */
+    public static Map<String, Object> producerProps(final String bootstrapServers) {
+        final Map<String, Object> map = new HashMap<>();
+        map.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        map.put(ProducerConfig.ACKS_CONFIG, "all");
+        map.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, "3000");
+        map.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, "3000");
+        map.put(ProducerConfig.LINGER_MS_CONFIG, "0");
+        map.put(ProducerConfig.RETRIES_CONFIG, "0");
+        map.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+        map.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        return map;
+    }
+
+    /**
+     * Set up test properties for an admin client.
+     * @param bootstrapServers the bootstrapServers property.
+     * @return the properties.
+     */
+    public static Map<String, Object> adminProps(final String bootstrapServers) {
+        final Map<String, Object> map = new HashMap<>();
+        map.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        return map;
+    }
+
+}
--- src/test/java/no/shhsoft/k3aembedded/AbstractK3aEmbeddedTest.java
+++ src/test/java/no/shhsoft/k3aembedded/AbstractK3aEmbeddedTest.java
@@ -1,23 +1,17 @@
 package no.shhsoft.k3aembedded;
 
-import org.apache.kafka.clients.CommonClientConfigs;
 import org.apache.kafka.clients.consumer.Consumer;
-import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
-import org.apache.kafka.common.serialization.IntegerDeserializer;
-import org.apache.kafka.common.serialization.IntegerSerializer;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.time.Duration;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
@@ -33,8 +27,8 @@ public abstract class AbstractK3aEmbeddedTest {
 
     @Test
     public void shouldProduceAndConsume() {
-        try (final Producer<Integer, Integer> producer = getProducer()) {
-            try (final Consumer<Integer, Integer> consumer = getConsumer()) {
+        try (final Producer<Integer, String> producer = getProducer()) {
+            try (final Consumer<Integer, String> consumer = getConsumer()) {
                 consumer.subscribe(Collections.singleton(TOPIC));
                 produce(producer);
                 final int consumedValue = consume(consumer);
@@ -43,37 +37,20 @@ public abstract class AbstractK3aEmbeddedTest {
         }
     }
 
-    public Producer<Integer, Integer> getProducer() {
-        final Map<String, Object> map = getCommonConfig();
-        map.put(ProducerConfig.ACKS_CONFIG, "all");
-        map.put(ProducerConfig.DELIVERY_TIMEOUT_MS_CONFIG, "3000");
-        map.put(ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, "3000");
-        map.put(ProducerConfig.LINGER_MS_CONFIG, "0");
-        map.put(ProducerConfig.RETRIES_CONFIG, "1");
-        map.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
-        map.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+    public Producer<Integer, String> getProducer() {
+        final Map<String, Object> map = K3aTestUtils.producerProps(getBootstrapServers());
+        map.putAll(getAdditionalClientConfig());
         return new KafkaProducer<>(map);
     }
 
-    private Consumer<Integer, Integer> getConsumer() {
-        final Map<String, Object> map = getCommonConfig();
-        map.put(ConsumerConfig.GROUP_ID_CONFIG, AbstractK3aEmbeddedTest.CONSUMER_GROUP_ID);
-        map.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
-        map.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
-        map.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
-        map.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+    private Consumer<Integer, String> getConsumer() {
+        final Map<String, Object> map = K3aTestUtils.consumerProps(getBootstrapServers(), AbstractK3aEmbeddedTest.CONSUMER_GROUP_ID, false);
+        map.putAll(getAdditionalClientConfig());
         return new KafkaConsumer<>(map);
     }
 
-    private Map<String, Object> getCommonConfig() {
-        final Map<String, Object> map = new HashMap<>();
-        map.put(CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG, getBootstrapServers());
-        map.putAll(getAdditionalClientConfig());
-        return map;
-    }
-
-    private void produce(final Producer<Integer, Integer> producer) {
-        final ProducerRecord<Integer, Integer> record = new ProducerRecord<>(TOPIC, 0, ++lastProducedValue);
+    private void produce(final Producer<Integer, String> producer) {
+        final ProducerRecord<Integer, String> record = new ProducerRecord<>(TOPIC, 0, String.valueOf(++lastProducedValue));
         try {
             producer.send(record, (metadata, exception) -> {
                 if (exception != null) {
@@ -87,12 +64,11 @@ public abstract class AbstractK3aEmbeddedTest {
         producer.flush();
     }
 
-    private int consume(final Consumer<Integer, Integer> consumer) {
+    private int consume(final Consumer<Integer, String> consumer) {
         int lastValue = -1;
-        final ConsumerRecords<Integer, Integer> records = consumer.poll(Duration.ofMillis(5000));
-        System.out.println("************ got " + (records.iterator().hasNext() ? "some" : "none"));
-        for (final ConsumerRecord<Integer, Integer> record : records) {
-            lastValue = record.value();
+        final ConsumerRecords<Integer, String> records = consumer.poll(Duration.ofMillis(5000));
+        for (final ConsumerRecord<Integer, String> record : records) {
+            lastValue = Integer.valueOf(record.value());
             consumer.commitAsync();
         }
         return lastValue;

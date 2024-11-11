7d2734e81debcb4c76803dcb4873cb445b1371bb
Sverre H. Huseby <shh@thathost.com>
2024-11-11 10:46:12 +0100

    fix(deps): add support for Kafka 3.9.0 (#140)

--- src/main/java/no/shhsoft/k3aembedded/FileUtils.java
+++ src/main/java/no/shhsoft/k3aembedded/FileUtils.java
@@ -4,6 +4,9 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 final class FileUtils {
 
@@ -34,6 +37,26 @@ final class FileUtils {
     private static void delete(final File fileOrDirectory) {
         if (!fileOrDirectory.delete()) {
             System.err.println("Unable to delete \"" + fileOrDirectory + "\". Ignoring.");
+        }
+    }
+
+    public static int execClass(final Class<?> target, final String... args) {
+        final List<String> command = new ArrayList<>();
+        command.add(System.getProperty("java.home") + File.separator + "bin" + File.separator + "java");
+        command.add("-cp");
+        command.add(System.getProperty("java.class.path"));
+        command.add(target.getName());
+        if (args != null) {
+            command.addAll(Arrays.asList(args));
+        }
+        try {
+            final Process process = new ProcessBuilder(command).start();
+            process.waitFor();
+            System.out.println(new String(process.getInputStream().readAllBytes()));
+            System.err.println(new String(process.getErrorStream().readAllBytes()));
+            return process.exitValue();
+        } catch (final Exception e) {
+            throw new RuntimeException(e);
         }
     }
 
